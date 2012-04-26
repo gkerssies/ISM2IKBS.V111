f5280b5b68811daaa41c978d88dc19ea11c0fbc7
Thomas Baart <m.thomasbaart@gmail.com>
2012-04-26 13:21:29 +0200

    Deleted: Separate panel classes. Was getting way too complicated. Changed: InlogFrame.java. Moved setupFrame() method into the setup() method. Added: File constants so you can change filepaths in a central place.

--- InfoClient/src/gfy/InlogFrame.java
+++ InfoClient/src/gfy/InlogFrame.java
@@ -1,6 +1,10 @@
 package gfy;
 
+import UserInterface.ImgButton;
 import UserInterface.InputPanel;
+import UserInterface.TexturedPanel;
+import UserInterface.TexturedPasswordField;
+import UserInterface.TexturedTextField;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
@@ -14,26 +18,48 @@ import javax.swing.JFrame;
  */
 public class InlogFrame extends JFrame {
 
-  private InputPanel usernameInputPanel, passwordInputPanel, buttonPanel;
+  private InputPanel usernamePanel, passwordPanel, buttonPanel, loginFormPanel;
+  private TexturedPanel mainPanel;
+  private TexturedTextField usernameField;
+  private TexturedPasswordField passwordField;
+  private ImgButton cancelButton, loginButton;
   private Dimension inlogFrameDim, userMonitorDim;
+  private static final String fieldBackground = "resources/images/backgrounds/fieldPattern.png";
+  private static final String mainPanelBackground = "resources/images/backgrounds/mainPanelPattern.png";
+  private static final String userNameLabelImage = "resources/images/labels/usernameLabel.png";
+  private static final String passwordLabelImage = "resources/images/labels/passwordLabel.png";
+  private static final String cancelButtonImage = "resources/images/buttons/cancelButton.png";
+  private static final String loginButtonImage = "resources/images/buttons/loginButton.png";
 
   public InlogFrame() {
     super();
-    setupPanels();
-    setupFrame();
+    setup();
   }
 
-  private void setupPanels() {
-    usernameInputPanel = new InputPanel( "resources/images/labels/usernameLabel.png" );
-    
-    passwordInputPanel = new InputPanel( "resources/images/labels/passwordLabel.png" );
+  private void setup() {
+    mainPanel = new TexturedPanel( mainPanelBackground );
+    loginFormPanel = new InputPanel();
+    usernamePanel = new InputPanel( userNameLabelImage );
+    passwordPanel = new InputPanel( passwordLabelImage );
     buttonPanel = new InputPanel();
-    add(usernameInputPanel);
-    add(passwordInputPanel);
-    add(buttonPanel);
-  }
 
-  private void setupFrame() {
+    usernameField = new TexturedTextField( fieldBackground, 15 );
+    passwordField = new TexturedPasswordField( fieldBackground, 15 );
+
+    cancelButton = new ImgButton( cancelButtonImage );
+    loginButton = new ImgButton( loginButtonImage );
+
+    usernamePanel.add( usernameField );
+    passwordPanel.add( passwordField );
+    buttonPanel.add( cancelButton );
+    buttonPanel.add( loginButton );
+
+    loginFormPanel.add( usernamePanel );
+    loginFormPanel.add( passwordPanel );
+    mainPanel.add( loginFormPanel );
+    mainPanel.add( buttonPanel );
+    add( mainPanel );
+
     userMonitorDim = Toolkit.getDefaultToolkit().getScreenSize();
 
     setLayout( new GridLayout() );
@@ -41,10 +67,10 @@ public class InlogFrame extends JFrame {
     setVisible( true );
     pack();
     inlogFrameDim = getSize();
-    
+
     setLocation( ( userMonitorDim.width / 2 ) - ( inlogFrameDim.width / 2 ),
-               ( int ) ( userMonitorDim.height / 2.75 ) - ( inlogFrameDim.height / 2 ) );
-    
+                 ( int ) ( userMonitorDim.height / 2.75 ) - ( inlogFrameDim.height / 2 ) );
+
     setResizable( false );
     setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
   }
