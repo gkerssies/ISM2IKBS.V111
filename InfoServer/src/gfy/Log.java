d57edc371ef1134daf3d1d0159de395b2a8da0c0
Janssen-laptop <Janssen-laptop@AS-884583495384>
2012-04-26 20:13:42 +0200

    Testje van jormen janssen

---
+++ InfoServer/src/gfy/Log.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gfy;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jormen Janssen
+ * @version 0.1 - 26 april 2012
+ */
+public class Log {
+  
+  static ArrayList<String> logitem = new ArrayList<String>();
+  static ArrayList<String> javaexception = new ArrayList<String>();
+  static ArrayList<String> friendlyerror = new ArrayList<String>();
+  static ArrayList<LogType> jexception = new ArrayList<LogType>();
+  
+  
+}
---
+++ InfoServer/src/gfy/Server.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gfy;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Jormen Janssen
+ * @version 0.1 - 26 april 2012
+ */
+public class Server extends Thread{
+
+  Config config;
+  ArrayList<Client> client;
+  ServerSocket serversocket;
+  /**
+   * Constructor for server
+   * @param config for the server
+   */
+  public Server( Config config ){
+    this.config = config;
+    client = new ArrayList<Client>();
+  }
+  
+  /**
+   * Method for multithreaded code to execute (implements Runable).
+   * @param config for the server
+   */
+  @Override
+  public void run()
+  {
+    try {
+      serversocket = new ServerSocket(config.getServerport());
+    } catch ( IOException ex ) {
+      Logger.getLogger( Server.class.getName() ).log( Level.SEVERE, null, ex );
+    }
+  }
+  
+}
