cab562e79cb1526f5ab4ea8d9ba67b1eb6ed8999
Administrator <Administrator@145.44.88.241>
2012-04-26 19:27:55 +0200

    Added InformationPanel class

---
+++ InfoServer/src/UserInterface/InformationPanel.java
@@ -0,0 +1,23 @@
+package UserInterface;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author Ido Bosman <ido_bosman@hotmail.com>
+ */
+public class InformationPanel extends JPanel {
+
+  private JLabel connectedClients;
+
+  /**
+   * Constructor for the InformationPanel class.
+   */
+  public InformationPanel() {
+    // Create label with information about the number of connected clients
+    connectedClients = new JLabel( "Aantal verbonden clients: 23" );
+    connectedClients.setHorizontalTextPosition( JLabel.CENTER );
+    connectedClients.setFont( null );
+    add( connectedClients );
+  }
+}
