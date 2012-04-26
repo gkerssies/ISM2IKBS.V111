5a9b917833245900896a3a158467422dd23e6acc
Thomas Baart <m.thomasbaart@gmail.com>
2012-04-26 12:04:23 +0200

    Added: InputPanel.java. Creates a rightly aligned panel with transparent background, and optionally a label from an image file.

---
+++ InfoClient/src/UserInterface/InputPanel.java
@@ -0,0 +1,38 @@
+package UserInterface;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author thomasbaart
+ */
+public class InputPanel extends JPanel {
+
+  private JLabel label;
+
+  /**
+   * Default constructor. Creates a right aligned panel with a fully transparent
+   * background.
+   */
+  public InputPanel() {
+    super( new FlowLayout( FlowLayout.RIGHT ) );
+    setBackground( new Color( 0, 0, 0, 0 ) );
+  }
+
+  /**
+   * Constructor. Creates a right aligned panel with a fully transparent
+   * background and a label.
+   *
+   * @param fileName Image file to make the label from.
+   */
+  public InputPanel( String fileName ) {
+    super( new FlowLayout( FlowLayout.RIGHT ) );
+    setBackground( new Color( 0, 0, 0, 0 ) );
+    label = new JLabel( new ImageIcon( fileName ) );
+    this.add( label );
+  }
+}
