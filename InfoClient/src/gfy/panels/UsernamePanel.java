14ff57cefecbb0add1edc4a1483183a4a6e36ab7
Thomas Baart <m.thomasbaart@gmail.com>
2012-04-26 12:47:51 +0200

    Added: UsernamePanel.java. One of the panels to go into LoginPanel.java.

---
+++ InfoClient/src/gfy/panels/UsernamePanel.java
@@ -0,0 +1,49 @@
+package gfy.panels;
+
+import UserInterface.InputPanel;
+import UserInterface.TexturedTextField;
+import java.awt.Color;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.LineBorder;
+
+/**
+ * UsernamePanel containing JLabel and TexturedTextField for InlogFrame.java
+ *
+ * @author thomasbaart
+ */
+public class UsernamePanel extends InputPanel {
+
+  private TexturedTextField inputField;
+
+  /**
+   * Default constructor.
+   */
+  public UsernamePanel() {
+    super();
+    generateTextField();
+  }
+
+  /**
+   *
+   * @param fileName Image file to make the label from.
+   */
+  public UsernamePanel( String fileName ) {
+    super( fileName );
+    generateTextField();
+  }
+
+  /**
+   * Generates the TextField for the user to enter his username into.
+   *
+   * @return
+   */
+  public final TexturedTextField generateTextField() {
+    inputField = new TexturedTextField( "resources/images/backgrounds/fieldPattern.png", 15 );
+    LineBorder outerBorder = new LineBorder( Color.gray, 1 );
+    LineBorder innerBorder = new LineBorder( new Color( 0, 0, 0, 0 ), 3 );
+    CompoundBorder compoundBorder = new CompoundBorder( outerBorder, innerBorder );
+    inputField.setBorder( compoundBorder );
+    add( inputField );
+    return inputField;
+  }
+}
