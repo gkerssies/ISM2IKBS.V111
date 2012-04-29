7d804234def663ec44a3e17c5f71e5ec1b0ebce4
Janssen-laptop <Janssen-laptop@AS-884583495384>
2012-04-29 14:50:48 +0200

    Testje van jormen janssen

---
+++ InfoServer/src/gfy/Protocol.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gfy;
+
+/**
+ *
+ * @author Jormen Janssen
+ * @version 0.1 - 29 april 2012
+ */
+class Protocol {
+  
+}
