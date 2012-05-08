6726b73ae2f2c9cba1e2db23c005fbc3ce6a2012
Administrator <Administrator@145.44.88.241>
2012-05-08 17:43:07 +0200

    Added functionality to NavQuery and NavQueryOverview classes.

--- InfoServer/src/gfy/NavQueryOverview.java
+++ InfoServer/src/gfy/NavQueryOverview.java
@@ -11,8 +11,28 @@ import java.util.ArrayList;
  */
 public class NavQueryOverview implements Serializable {
 
-  private ArrayList<NavQuery> navQuerys;
+  private ArrayList<NavQuery> navQueries;
 
+  /**
+   * Constructor for the NavQueryOverview class.
+   */
   public NavQueryOverview() {
+    navQueries = new ArrayList<NavQuery>();
+  }
+
+  /**
+   *
+   * @param nq object with detailled query data
+   */
+  public void addNavQuery( NavQuery nq ) {
+    navQueries.add( nq );
+  }
+
+  /**
+   *
+   * @param nq object with detailled query data
+   */
+  public void deleteNavQuery( NavQuery nq ) {
+    navQueries.remove( nq );
   }
 }
