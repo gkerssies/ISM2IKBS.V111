07c70c4416cfc6b29d060f7abc3ffe3c4be8a2e7
Thomas Baart <m.thomasbaart@gmail.com>
2012-05-08 10:50:52 +0200

    Added: JavaDoc comments in WrappableJLabel.java

--- InfoClient/src/UserInterface/WrappableJLabel.java
+++ InfoClient/src/UserInterface/WrappableJLabel.java
@@ -3,14 +3,33 @@ package UserInterface;
 import java.awt.Dimension;
 import javax.swing.JLabel;
 
+/**
+ * Modified JLabel that is able to wrap around.
+ *
+ * @author thomasbaart
+ */
 public class WrappableJLabel extends JLabel {
 
   private final int preferredWidth;
 
+  /**
+   * Constructor of WrappableJLabel. Note that the preferredWidth isn't honored
+   * in all the layoutmanagers.
+   *
+   * @param preferredWidth The preferred width in pixels.
+   */
   public WrappableJLabel( final int preferredWidth ) {
     this.preferredWidth = preferredWidth;
   }
 
+  /**
+   * Constructor of WrappableJLabel, directly sets the labels text. Note that
+   * the preferredWidth isn't honored
+   * in all the layoutmanagers.
+   *
+   * @param string         The text to set this label to.
+   * @param preferredWidth The preferred width in pixels.
+   */
   public WrappableJLabel( String string, final int preferredWidth ) {
     setText( string );
     this.preferredWidth = preferredWidth;
