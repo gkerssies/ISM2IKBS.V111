43fbfba70c78a807e0c03ff6ac724ab5d1246f75
Gerjan <Gerjan@145.44.83.63>
2012-05-08 16:05:28 +0200

    UserType.java added to Masterclient

---
+++ MasterClient/src/gfy/UserType.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gfy;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Janssen-laptop
+ */
+public enum UserType implements Serializable{
+  beheerder,gebruiker
+}
