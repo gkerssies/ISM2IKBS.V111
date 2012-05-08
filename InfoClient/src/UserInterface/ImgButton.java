25d2782a44b1fb4c2e76c60e7d77aa2150460e14
Thomas Baart <m.thomasbaart@gmail.com>
2012-05-08 10:51:28 +0200

    Added: JavaDoc comments in ImgButton.java

--- InfoClient/src/UserInterface/ImgButton.java
+++ InfoClient/src/UserInterface/ImgButton.java
@@ -4,10 +4,14 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 /**
+ * Modified JButton that uses an image instead of text.
  *
  * @author thomasbaart
  */
 public class ImgButton extends JButton {
+  /*
+   * Bug: Clicking an ImgButton makes the transparant pixels opaque.
+   */
 
   /**
    * Default constructor for ImgButton. Generates a normal JButton.
