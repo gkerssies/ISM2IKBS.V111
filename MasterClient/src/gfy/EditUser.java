bda50a63a894f976c2246c9fec5a65bb8dacb63b
Gerjan <Gerjan@145.44.83.63>
2012-05-10 11:34:48 +0200

    Added EditUser class

---
+++ MasterClient/src/gfy/EditUser.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gfy;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author Gerjan Kerssies
+ */
+public class EditUser extends JFrame implements ActionListener {
+
+  public EditUser() {
+  }
+
+  public void actionPerformed( ActionEvent e ) {
+  }
+}
