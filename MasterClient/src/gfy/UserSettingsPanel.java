d2fd1c6e74a309eaaa6145121f145e872ed0af6b
Gerjan <Gerjan@145.44.83.63>
2012-05-10 12:15:23 +0200

    Small change

--- MasterClient/src/gfy/UserSettingsPanel.java
+++ MasterClient/src/gfy/UserSettingsPanel.java
@@ -60,8 +60,8 @@ public class UserSettingsPanel extends JPanel {
         break;
         
       case "editUser":
-        buttonCancel.addActionListener( EditUser() );
-        buttonSave.addActionListener( EditUser() );
+        //buttonCancel.addActionListener( EditUser() );
+        //buttonSave.addActionListener( EditUser() );
         break;
     }
     
