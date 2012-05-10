6969a6b3f433001c0ffacb73753b0426039bbb96
Gerjan <Gerjan@145.44.83.63>
2012-05-10 12:02:50 +0200

    Added UserSettingsPanel class

---
+++ MasterClient/src/gfy/UserSettingsPanel.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gfy;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author Gerjan
+ */
+public class UserSettingsPanel extends JPanel {
+  public UserSettingsPanel() {
+    
+  }
+}
