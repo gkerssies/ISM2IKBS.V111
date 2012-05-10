51b69eebb46fa5beb69647bcfe57b5f9d6f8cda5
Janssen-laptop <Janssen-laptop@AS-884583495384>
2012-05-10 11:49:02 +0200

    added support for transfering users + save and load them

---
+++ MasterClient/src/gfy/EmptyFrame.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gfy;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Janssen-laptop
+ */
+public class EmptyFrame extends JFrame{
+  
+}
