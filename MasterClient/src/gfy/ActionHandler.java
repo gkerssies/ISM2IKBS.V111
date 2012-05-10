c75a8e61054134a138e0ed4915698ce3abb18601
Gerjan <Gerjan@145.44.83.63>
2012-05-10 12:33:55 +0200

    Added ActionHandler

---
+++ MasterClient/src/gfy/ActionHandler.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gfy;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author Gerjan Kerssies
+ */
+public class ActionHandler implements ActionListener {
+  public ActionHandler() {
+    
+  }
+  
+  public void actionPerformed( ActionEvent e ) {
+    
+  }
+}
