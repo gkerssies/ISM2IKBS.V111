3ed57a14ec390888b3ed5baf780fb66c4bec3b22
Gerjan <Gerjan@145.44.83.63>
2012-05-20 21:06:21 +0200

    Changed AddNavQuery - added SQLManagement - added NavQuerySettingsPanel

--- MasterClient/src/gfy/AddNavQuery.java
+++ MasterClient/src/gfy/AddNavQuery.java
@@ -12,24 +12,40 @@ import javax.swing.*;
  */
 public class AddNavQuery extends JFrame implements ActionListener {
 
-  private JPanel panel, panelLabels, panelInput;
-  
+  private SQLManagement sqlManagement;
+  private NavQuerySettingsPanel panel;
+
   /**
    * Constructor for the AddNavQuery class.
+   *
+   * @param sqlManagement the SQLManagement object
    */
-  public AddNavQuery() {
-    panel = new JPanel();
-    
+  public AddNavQuery( SQLManagement sqlManagement ) {
+    this.sqlManagement = sqlManagement;
+    panel = new NavQuerySettingsPanel( "addNavQuery", this, 0 );
+
     setLayout( new GridLayout( 4, 1 ) );
     setContentPane( panel );
-    setSize( 350, 220 );
+    setSize( 310, 280 );
     setResizable( false );
     setTitle( "Navision query toevoegen" );
     setLocationRelativeTo( getRootPane() );
     setVisible( true );
   }
 
+  /**
+   * Save the new NavQuery.
+   */
+  public void save() {
+  }
+
   @Override
   public void actionPerformed( ActionEvent e ) {
+    if ( e.getSource() == panel.getButtonCancel() ) {
+      dispose();
+    } else if ( e.getSource() == panel.getButtonSave() ) {
+      save();
+      dispose();
+    }
   }
 }
