129eadc6cb344f54ec5da731e2a8d32a41730ce4
Gerjan <Gerjan@145.44.83.63>
2012-05-20 18:18:50 +0200

    Added DeleteNavQuery.java

---
+++ MasterClient/src/gfy/DeleteNavQuery.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gfy;
+
+/**
+ * Removes the selected NavQuery.
+ *
+ * @author Gerjan Kerssies
+ */
+public class DeleteNavQuery {
+
+  /**
+   * Constructor for the EditNavQuery class.
+   */
+  public DeleteNavQuery() {
+  }
+}
