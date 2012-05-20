b76af3a88c753600b982c08419e9d8e71fdb814c
Gerjan <Gerjan@145.44.83.63>
2012-05-20 18:18:20 +0200

    Added AddNavQuery.java

---
+++ MasterClient/src/gfy/AddNavQuery.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gfy;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+/**
+ * Creates a JFrame which a NavQuery can be added.
+ *
+ * @author Gerjan Kerssies
+ */
+public class AddNavQuery extends JFrame implements ActionListener {
+
+  /**
+   * Constructor for the AddNavQuery class.
+   */
+  public AddNavQuery() {
+  }
+
+  @Override
+  public void actionPerformed( ActionEvent e ) {
+  }
+}
