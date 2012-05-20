c199307ab031ca2ec69d3c778a5e1c0ffde47d8c
Gerjan <Gerjan@145.44.83.63>
2012-05-20 21:06:44 +0200

    Changed EditNavQuery - added SQLManagement - added NavQuerySettingsPanel

--- MasterClient/src/gfy/EditNavQuery.java
+++ MasterClient/src/gfy/EditNavQuery.java
@@ -3,8 +3,7 @@ package gfy;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 /**
  * Creates a JFrame which a NavQuery can be edited.
@@ -13,24 +12,40 @@ import javax.swing.JPanel;
  */
 public class EditNavQuery extends JFrame implements ActionListener {
 
-  private JPanel panel;
+  private SQLManagement sqlManagement;
+  private NavQuerySettingsPanel panel;
 
   /**
    * Constructor for the EditNavQuery class.
+   *
+   * @param sqlManagement the SQLManagement object
    */
-  public EditNavQuery() {
-    panel = new JPanel();
-    
+  public EditNavQuery( SQLManagement sqlManagement ) {
+    this.sqlManagement = sqlManagement;
+    panel = new NavQuerySettingsPanel( "editNavQuery", this, 0 );
+
     setLayout( new GridLayout( 4, 1 ) );
     setContentPane( panel );
-    setSize( 350, 220 );
+    setSize( 310, 280 );
     setResizable( false );
     setTitle( "Navision query wijzigen" );
     setLocationRelativeTo( getRootPane() );
     setVisible( true );
   }
 
+  /**
+   * Save the changed NavQuery.
+   */
+  public void save() {
+  }
+
   @Override
   public void actionPerformed( ActionEvent e ) {
+    if ( e.getSource() == panel.getButtonCancel() ) {
+      dispose();
+    } else if ( e.getSource() == panel.getButtonSave() ) {
+      save();
+      dispose();
+    }
   }
 }
