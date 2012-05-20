dc50c93876789cf079780b1812f7a2e22c3ca40b
Gerjan <Gerjan@145.44.83.63>
2012-05-20 12:55:51 +0200

    Added javadoc to DeleteUser

--- MasterClient/src/gfy/DeleteUser.java
+++ MasterClient/src/gfy/DeleteUser.java
@@ -1,7 +1,8 @@
 package gfy;
 
 /**
- *
+ * Removes the selected user.
+ * 
  * @author Gerjan Kerssies
  */
 public class DeleteUser {
@@ -9,6 +10,14 @@ public class DeleteUser {
   private User user;
   private AuthorizationManagement authorizationManagement;
 
+  /**
+   * Constructor for the DeleteUser class.
+   * Removes the selected user ftom the User object.
+   * 
+   * @param user                    the User object
+   * @param username                the username of the clicked user
+   * @param authorizationManagement the AuthorizationManagement object
+   */
   public DeleteUser( User user, String username, AuthorizationManagement authorizationManagement ) {
     this.user = user;
     this.authorizationManagement = authorizationManagement;
