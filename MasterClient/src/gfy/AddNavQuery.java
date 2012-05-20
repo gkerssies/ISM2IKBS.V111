1a2f7f95a3e655ddf89ba2ef82ccecc6f32e6bf5
Gerjan <Gerjan@145.44.83.63>
2012-05-20 18:38:21 +0200

    Changed AddNavQuery - deleted top comment

--- MasterClient/src/gfy/AddNavQuery.java
+++ MasterClient/src/gfy/AddNavQuery.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package gfy;
 
 import java.awt.GridLayout;
@@ -16,7 +12,7 @@ import javax.swing.*;
  */
 public class AddNavQuery extends JFrame implements ActionListener {
 
-  private JPanel panel;
+  private JPanel panel, panelLabels, panelInput;
   
   /**
    * Constructor for the AddNavQuery class.
