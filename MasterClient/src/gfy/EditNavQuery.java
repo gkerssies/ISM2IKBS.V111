897ab449e908b1180a1831d392952c9c80294903
Gerjan <Gerjan@145.44.83.63>
2012-05-20 18:18:33 +0200

    Added EditNavQuery.java

---
+++ MasterClient/src/gfy/EditNavQuery.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gfy;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+
+/**
+ * Creates a JFrame which a NavQuery can be edited.
+ *
+ * @author Gerjan Kerssies
+ */
+public class EditNavQuery extends JFrame implements ActionListener {
+
+  /**
+   * Constructor for the EditNavQuery class.
+   */
+  public EditNavQuery() {
+  }
+
+  @Override
+  public void actionPerformed( ActionEvent e ) {
+  }
+}
