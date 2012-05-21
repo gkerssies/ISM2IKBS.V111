c44316ca3ed57ed78371e107a7214d1aa89e0f7a
Gerjan <Gerjan@145.44.83.63>
2012-05-21 21:09:54 +0200

    Changed AddNavQuery - save new user is now working

--- MasterClient/src/gfy/AddNavQuery.java
+++ MasterClient/src/gfy/AddNavQuery.java
@@ -11,19 +11,23 @@ import javax.swing.*;
  * @author Gerjan Kerssies
  */
 public class AddNavQuery extends JFrame implements ActionListener {
-
+  
   private SQLManagement sqlManagement;
   private NavQuerySettingsPanel panel;
+  private NavQueryOverview nqo;
 
   /**
    * Constructor for the AddNavQuery class.
    *
    * @param sqlManagement the SQLManagement object
+   * @param nqo           the NavQueryOverview object
    */
-  public AddNavQuery( SQLManagement sqlManagement ) {
+  public AddNavQuery( SQLManagement sqlManagement, NavQueryOverview nqo ) {
     this.sqlManagement = sqlManagement;
+    this.nqo = nqo;
+    
     panel = new NavQuerySettingsPanel( "addNavQuery", this, 0 );
-
+    
     setLayout( new GridLayout( 4, 1 ) );
     setContentPane( panel );
     setSize( 310, 280 );
@@ -35,17 +39,34 @@ public class AddNavQuery extends JFrame implements ActionListener {
 
   /**
    * Save the new NavQuery.
+   *
+   * @return boolean user object is saved
    */
-  public void save() {
+  public boolean save() {
+    if ( panel.getTitle().getText().equals( "" ) ) {
+      JOptionPane.showMessageDialog( panel, "Titel veld kan niet leeg zijn", "Validatie fout", JOptionPane.INFORMATION_MESSAGE );
+      return false;
+    } else if ( panel.getDescription().getText().equals( "" ) ) {
+      JOptionPane.showMessageDialog( panel, "Beschrijving veld kan niet leeg zijn", "Validatie fout", JOptionPane.INFORMATION_MESSAGE );
+      return false;
+    } else if ( panel.getQuery().getText().equals( "" ) ) {
+      JOptionPane.showMessageDialog( panel, "Query veld kan niet leeg zijn", "Validatie fout", JOptionPane.INFORMATION_MESSAGE );
+      return false;
+    } else {
+      NavQuery nq = new NavQuery( WIDTH, panel.getTitle().getText(), panel.getDescription().getText(), panel.getQuery().getText() );
+      nqo.addNavQuery( nq );
+      return true;
+    }
   }
-
+  
   @Override
   public void actionPerformed( ActionEvent e ) {
     if ( e.getSource() == panel.getButtonCancel() ) {
       dispose();
     } else if ( e.getSource() == panel.getButtonSave() ) {
-      save();
-      dispose();
+      if ( save() ) {
+        dispose();
+      }
     }
   }
 }
