314023a26d67aeec9cf097682ace5c2a444a7a19
Janssen-laptop <Janssen-laptop@AS-884583495384>
2012-05-22 10:35:14 +0200

    Removed ugly sout

--- InfoClient/src/view/dataview/DataviewFrame.java
+++ InfoClient/src/view/dataview/DataviewFrame.java
@@ -15,46 +15,41 @@ public class DataviewFrame extends JFrame {
   private NavQueryResultSet nqrs;
   private NavQuery nvq;
 
-  public DataviewFrame(NavQueryResultSet nqrs,NavQuery nvq) {
+  public DataviewFrame( NavQueryResultSet nqrs, NavQuery nvq ) {
     super();
-    setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+    setDefaultCloseOperation( DISPOSE_ON_CLOSE );
     this.nqrs = nqrs;
     this.nvq = nvq;
     setup();
-    
+
     pack();
     setLocationRelativeTo( getRootPane() );
   }
 
   private void setup() {
-    setSize(700,500);
-    this.setTitle(nvq.getTitle());
+    setSize( 700, 500 );
+    this.setTitle( nvq.getTitle() );
     tabbedPane = new JTabbedPane();
-    
-    JTable table = new JTable( nqrs.getRow(), nqrs.getColumns());
-    JScrollPane scrollPane = new JScrollPane(table);
-    
-    
-    JPanel datatable = new JPanel();
-    datatable.setLayout(new FlowLayout());
-    scrollPane.add(datatable);
-    GraphPanel gp = null;
-     GraphPanel gp2 = null;
-    
-    if (nvq.getId() == 0)
-    {
-       gp = new GraphPanel(nqrs,nvq,2,1);
-       gp2 = new GraphPanel(nqrs,nvq,2,2);
-    }
-    else
-    {
-      gp = new GraphPanel(nqrs,nvq,1,1);
-      gp2 = new GraphPanel(nqrs,nvq,1,2);
-    }
-    tabbedPane.addTab( "Data", scrollPane);
-    tabbedPane.addTab( "Grafiek (Staaf diagram)", gp);
-    tabbedPane.addTab( "Grafiek (Taart diagram)", gp2);
-    add(tabbedPane);
-  }
 
+    JTable table = new JTable( nqrs.getRow(), nqrs.getColumns() );
+    JScrollPane scrollPane = new JScrollPane( table );
+
+
+    JPanel datatable = new JPanel();
+    datatable.setLayout( new FlowLayout() );
+    scrollPane.add( datatable );
+    GraphPanel gp = null;
+    GraphPanel gp2 = null;
+    tabbedPane.addTab( "Data", scrollPane );
+    if ( nvq.isBuiltInQuery() ) {
+      gp = new GraphPanel( nqrs, nvq, 1, 1 );
+      gp2 = new GraphPanel( nqrs, nvq, 1, 2 );
+      tabbedPane.addTab( "Grafiek (Staaf diagram)", gp );
+      tabbedPane.addTab( "Grafiek (Taart diagram)", gp2 );
+    }
+
+    
+
+    add( tabbedPane );
+  }
 }
